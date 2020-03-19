a43ae65fc8e54cbb2f8c6a26ceb47735f32822de
BarteKKmita <bartekkmitaa@gmail.com>
2020-03-19 18:31:13 +0100

    Refactor class responsible user token generation

--- src/main/java/com/learning/securitydocker/jwtauth/JwtUserCredentialsFilter.java
+++ src/main/java/com/learning/securitydocker/jwtauth/JwtUserCredentialsFilter.java
@@ -2,13 +2,14 @@ package com.learning.securitydocker.jwtauth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,10 +19,14 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class JwtUserCredentialsFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
     private final AuthenticationManager authenticationManager;
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public JwtUserCredentialsFilter(AuthenticationManager authenticationManager){
+    public JwtUserCredentialsFilter(@Autowired JwtConfig jwtConfig, @Autowired SecretKey secretKey, AuthenticationManager authenticationManager){
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
         this.authenticationManager = authenticationManager;
     }
 
@@ -30,7 +35,7 @@ public class JwtUserCredentialsFilter extends UsernamePasswordAuthenticationFilt
                                                 HttpServletResponse response) throws AuthenticationException{
         try {
             var user = OBJECT_MAPPER.readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
-            var authenticationToken = new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword());
+            Authentication authenticationToken = new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword());
             return authenticationManager.authenticate(authenticationToken);
         } catch (IOException e) {
             throw new RuntimeException("Cannot read request!", e);
@@ -40,18 +45,17 @@ public class JwtUserCredentialsFilter extends UsernamePasswordAuthenticationFilt
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult) throws IOException, ServletException{
-        var usersToken = getToken(authResult);
-        response.addHeader("Authorization", "Bearer " + usersToken);
+        var usersToken = getJwsToken(authResult);
+        response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + usersToken);
     }
 
-    private String getToken(Authentication authResult){
-        var secureKey = "securesecuresecuresecuresecuresecuresecuresecure";
+    private String getJwsToken(Authentication authResult){
         return Jwts.builder()
                 .setSubject(authResult.getName())
                 .claim("authorities", authResult.getAuthorities())
                 .setIssuedAt(new Date())
                 .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(1)))
-                .signWith(Keys.hmacShaKeyFor(secureKey.getBytes()))
+                .signWith(secretKey)
                 .compact();
     }
 }
